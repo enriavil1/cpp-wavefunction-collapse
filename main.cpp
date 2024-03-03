7c4e7b5f5a469ccd04d483ce9fac1891282b0dc4
Enrique Jose Avila Asapche <eavilaasapche@gmail.com>
2024-03-03 15:33:32 +0000

    creating sdl2 window

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
-
 #include <iostream>
 
-int main() { std::cout << "Hello World!" << std::endl; }
+#include <SDL2/SDL.h>
+
+int main() {
+  SDL_Window *window = nullptr;
+
+  if (SDL_Init(SDL_INIT_VIDEO)) {
+    std::cout << "Failed to initialize SDL Video: " << SDL_GetError()
+              << std::endl;
+    return -1;
+  }
+
+  window = SDL_CreateWindow("CPP Wavefunction Collapse", SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+  if (window == nullptr) {
+    std::cout << "Failed to create SDL window " << SDL_GetError() << std::endl;
+    return -1;
+  }
+
+  SDL_UpdateWindowSurface(window);
+  SDL_Delay(20000);
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
